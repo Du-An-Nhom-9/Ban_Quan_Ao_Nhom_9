a500818e4715d63b5c355a591bb90c0875961454
Pham Van duy <duypvph24281@fpt.edu.vn>
2023-06-12 15:51:05 +0700

    check quyền

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -8,28 +8,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
-  submitted: boolean = false;
-  formSignin = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.minLength(6)]]
-  });
-
-  constructor(private fb: FormBuilder, private auth: AuthService,private router: Router) {
-
-  }
-
-  onHandleSubmit() {
-    this.submitted = true;
-    if (this.formSignin.valid) {
-      console.log(1)
-
-      this.auth.signin(this.formSignin.value).subscribe(data => {
-        localStorage.setItem('user', JSON.stringify(data));
-        this.router.navigate(['/admin'])
-        return true
-      });
-    }
-    
-  }
+  
   
 }
