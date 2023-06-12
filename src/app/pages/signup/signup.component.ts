1038820cb95bc0d178dfcf77f2c7fc86d9449f9b
tranvuanh-ph24280 <115098767+tranvuanh-ph24280@users.noreply.github.com>
2023-06-12 16:00:44 +0700

    hoan thanh check quyen sigin

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -8,5 +8,32 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
- 
+  submitted: boolean = false;
+  formSignup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  }, { validators: this.checkPasswords });
+
+  constructor(private fb: FormBuilder, private auth: AuthService,private router:Router) {
+
+  }
+
+  checkPasswords(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    if (password === confirmPassword) return null;
+    return { notMatch: true };
+  }
+
+  onHandleSubmit() {
+    this.submitted = true;
+    if (this.formSignup.valid) {
+      this.auth.signup(this.formSignup.value).subscribe(data => {
+        console.log(data);
+        this.router.navigate(['/signin']);
+      });
+    }
+  }
 }
